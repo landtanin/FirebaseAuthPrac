a2de4d7efd63c5db3bf84b92467cad775595a6e1
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-07-29 18:06:22 +0700

    Update Display name

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,7 @@
 package com.landtanin.firebaseauthprac;
 
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.landtanin.firebaseauthprac.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     private String email;
-    private String password;
+    private String password, update;
 
     private final String TAG = MainActivity.class.getSimpleName();
 
@@ -45,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 if (user != null) {
                     // User is signed in
                     binding.signInInfo.setText(user.getEmail());
+//                    binding.signInInfo.setText(user.getDisplayName());
 
                 } else {
                     // User is signed out
@@ -78,6 +81,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        binding.updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                update = binding.updateDisField.getText().toString();
+
+                updateUser();
+            }
+        });
+
+
+    }
+
+    private void updateUser() {
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(update)
+                .setPhotoUri(Uri.parse("http://placehold.it/96x96"))
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            binding.disNameTxt.setText(user.getDisplayName());
+                            Log.d(TAG, "User profile updated.");
+                        } else {
+                            Toast.makeText(MainActivity.this, "No update data", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 
