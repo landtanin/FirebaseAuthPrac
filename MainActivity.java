54969791c7607216ae8f36e9318993edd0b10fd9
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-07-29 16:09:04 +0700

    Add new user account feature

---
+++ MainActivity.java
@@ -0,0 +1,99 @@
+package com.landtanin.firebaseauthprac;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.landtanin.firebaseauthprac.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private String email;
+    private String password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Waiting for the user to sign-in, if they sign-in, getCurrentUser will not null
+        // and we'll receive a data of the current user.
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+//                    createUser();
+
+                } else {
+                    // User is signed out
+                }
+
+            }
+
+        };
+
+
+        binding.createAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createUser();
+            }
+        });
+
+
+    }
+
+    private void createUser() {
+        mAuth.createUserWithEmailAndPassword(binding.userNameTxt.getText().toString(), binding.passTxt.getText().toString())
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    // Validate user account
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    } else {
+
+                    }
+
+                }
+            });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+
+    }
+}
